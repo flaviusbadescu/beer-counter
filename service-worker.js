b5fed14b6c1b813cc20eac4f1ba1724114d50ffd
flaviusbadescu <flaviusbadescu@gmail.com>
2026-06-17 21:11:56 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Bump this version string whenever you deploy changes so old caches get replaced.
-const CACHE_VERSION = 'beer-counter-v1';
+const CACHE_VERSION = 'beer-counter-v2';
 
 const APP_SHELL = [
   './',
